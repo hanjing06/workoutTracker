41e835caad3f93188b8efe4b038d89c8f8f5d536
hanjing06 <hanjinglin8@gmail.com>
2025-12-09 00:01:27 -0500

    Initial commit

---
+++ src/main/java/com/hanjing/workoutTracker/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.hanjing.workoutTracker.controller; //controllers handle signup/login requests and authorized requests
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class UserController {
+
+}
