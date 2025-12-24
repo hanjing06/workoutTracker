99005383095f436efc6a79fb382ace66d46eef5a
hanjing06 <hanjinglin8@gmail.com>
2025-12-24 17:45:24 -0500

    Added UserRepository

--- src/main/java/com/hanjing/workoutTracker/repositories/UserRepository.java
+++ src/main/java/com/hanjing/workoutTracker/repositories/UserRepository.java
@@ -2,7 +2,12 @@ package com.hanjing.workoutTracker.repositories;
 
 import com.hanjing.workoutTracker.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email); //access method that will return if user exists in database
+
 }
