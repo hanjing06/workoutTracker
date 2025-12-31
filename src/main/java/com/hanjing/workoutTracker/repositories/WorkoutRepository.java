65ca4ae2415f6495cfbfb2fc52b41cd1f150d1e6
hanjing06 <hanjinglin8@gmail.com>
2025-12-31 12:38:13 -0500

    Workout repo done

--- src/main/java/com/hanjing/workoutTracker/repositories/WorkoutRepository.java
+++ src/main/java/com/hanjing/workoutTracker/repositories/WorkoutRepository.java
@@ -1,4 +1,15 @@
 package com.hanjing.workoutTracker.repositories;
 
-public interface WorkoutRepository {
+import com.hanjing.workoutTracker.models.User;
+import com.hanjing.workoutTracker.models.Workout;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface WorkoutRepository extends JpaRepository<Workout, Long> {
+    List<Workout> findbyUserID(Long userId);
+    List<Workout> findByWorkout(String name);
+    List<Workout> findByDate(User user, LocalDateTime date);
+
 }
